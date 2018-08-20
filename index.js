d38ab8e2f7b0762249307851fa00217166494d35
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-20 13:47:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
+const moves = {
+  37: function(){ //left
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
  
     dodger.style.left = `${left - 1}px`
   }
+}
+
+document.addEventListener('keydown', function(e) {
+  moves[e.which]();
 })
