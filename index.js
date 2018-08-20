f09295e01a7d6b87e9da5a81b52325cba6bd79b6
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-20 13:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const moves = {
     const left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left - 1}px`
   },
-  38: function(){ //left
+  38: function(){ //up
+    const bottomNumbers = dodger.style.bottom.replace('px', '')
+    const bottom = parseInt(bottomNumbers, 10)
+    dodger.style.bottom = `${bottom + 1}px`
+  },
+  39: function(){ //right
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left + 1}px`
   },
-  39: function(){ //left
-    const leftNumbers = dodger.style.left.replace('px', '')
-    const left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left - 1}px`
-  },
-  40: function(){ //left
-    const leftNumbers = dodger.style.left.replace('px', '')
-    const left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left - 1}px`
-  },
+  40: function(){ //down
+    const bottomNumbers = dodger.style.bottom.replace('px', '')
+    const bottom = parseInt(bottomNumbers, 10)
+    dodger.style.bottom = `${bottom - 1}px`
+  }
 }
 
 document.addEventListener('keydown', function(e) {
