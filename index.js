a3ed0a43e065e625faa96242fc1a04d33af344a7
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-20 13:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const dodger = document.getElementById('dodger')
 const moves = {
+  36: function(){ //left
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left - 1}px`
+  },
   37: function(){ //left
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
- 
     dodger.style.left = `${left - 1}px`
-  }
+  },
+  37: function(){ //left
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left - 1}px`
+  },
+  37: function(){ //left
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left - 1}px`
+  },
 }
 
 document.addEventListener('keydown', function(e) {
