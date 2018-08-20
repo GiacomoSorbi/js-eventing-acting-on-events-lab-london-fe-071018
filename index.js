ac628041da13ac1a4f1f04e624750be7d2709584
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-20 13:49:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const dodger = document.getElementById('dodger')
 const moves = {
-  36: function(){ //left
-    const leftNumbers = dodger.style.left.replace('px', '')
-    const left = parseInt(leftNumbers, 10)
-    dodger.style.left = `${left - 1}px`
-  },
   37: function(){ //left
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left - 1}px`
   },
-  37: function(){ //left
+  38: function(){ //left
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left - 1}px`
   },
-  37: function(){ //left
+  39: function(){ //left
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left - 1}px`
+  },
+  40: function(){ //left
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
     dodger.style.left = `${left - 1}px`
